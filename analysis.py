cde5b86dd687e2a69346fe76794099e9664124f9
rabbott99 <rabbott4927@gmail.com>
2019-12-16 00:27:19 -0500

    Complete the analysis code

--- analysis.py
+++ analysis.py
@@ -1,22 +1,152 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from uncertainties import ufloat, unumpy
+import os
 
-def read_output(filename):
-    data = np.loadtxt(filename)
-    T = data[:,0].flatten()
-    # energy
-    E = data[:,1::3]
-    # specific heat
-    C = data[:,2::3]
-    # susceptibility
-    chi = data[:,3::3]
+class Output:
+    def __init__(self, filename):
+        data = np.loadtxt(filename)
+        self.T   = data[:,0].flatten()
+        self.E   = data[:,1]
+        self.C   = data[:,2]
+        self.chi = data[:,3]
+        self.M   = data[:,4]
 
-    return T, E, C, chi
+        offset = 4
+        self.E_err   = data[:,1 + offset]
+        self.C_err   = data[:,2 + offset]
+        self.chi_err = data[:,3 + offset]
+        self.M_err   = data[:,4 + offset]
+        
+L_values = [10, 16, 24, 36, 50]
+#L_values = L_values[:3]
 
-T, E, C, chi = read_output("out.txt")
+def get_filename(L):
+    return "L{}output.txt".format(L)
 
-E_mean = E[:,0]
-E_errs = E[:,1]
+def get_output(L):
+    return Output(get_filename(L))
 
-plt.errorbar(T, E_mean, yerr=E_errs)
-plt.show()
+def Tc_from_chi(output):
+    chi = output.chi
+    idx = np.argmax(chi)
+    return output.T[idx]
+
+def extrapolate_Tc_from_chi(L_vals = L_values):
+    Linv = 1 / np.array(L_vals, dtype=np.float)
+    Tc = [Tc_from_chi(get_output(L)) for L in L_vals]
+
+    _, intercept = np.polyfit(Linv, Tc, 1)
+    return intercept
+
+def chi_max(output):
+    chi = output.chi
+    chi_err = output.chi_err
+    idx = np.argmax(chi)
+    return ufloat(chi[idx], chi_err[idx])
+
+def gamma_over_nu(Lvals = L_values):
+    logL = np.log(Lvals)
+    logchi = np.log([chi_max(get_output(L)).nominal_value for L in Lvals])
+    slope, _ = np.polyfit(logL, logchi, 1)
+    return slope
+
+def get_M_at_Tc(L):
+    output = get_output(L)
+    return output.M[np.argmax(output.chi)]
+
+def beta_over_nu(Lvals = L_values):
+    logL = np.log(Lvals)
+    logM = np.log([get_M_at_Tc(L) for L in Lvals])
+    slope, _ = np.polyfit(logL, logM, 1)
+    return -slope
+
+def Tc_from_Cv(output):
+    C = output.C
+    idx = np.argmax(C)
+    return output.T[idx]
+
+def extrapolate_Tc_from_Cv(L_vals = L_values):
+    Linv = 1 / np.array(L_vals, dtype=np.float)
+    Tc = [Tc_from_Cv(get_output(L)) for L in L_vals]
+
+    _, intercept = np.polyfit(Linv, Tc, 1)
+    return intercept
+
+def Cv_max(output):
+    Cv = output.C
+    Cv_err = output.C_err
+    idx = np.argmax(Cv)
+    return ufloat(Cv[idx], Cv_err[idx])
+
+def entropy_at_idx(output, idx):
+    ret = ufloat(0,0)
+    T = output.T
+    for i in range(1,idx + 1):
+        C = ufloat(output.C[i], output.C_err[i])
+        dT = T[i] - T[i - 1]
+        ret += C / T[i] * dT
+    return ret
+
+def entropy(output):
+    ret = []
+    for i in range(len(output.T)):
+        ret.append(entropy_at_idx(output, i).nominal_value)
+    return np.array(ret)
+
+def free_energy(output):
+    S = entropy(output)
+    return output.E - output.T * S
+
+# Computes S(T = 4.5)
+def entropy_limit(L):
+    output = get_output(L)
+    return entropy_at_idx(output, len(output.T) - 1)
+
+if __name__ == '__main__':
+    # Ensure output director exists
+    output_dir = "results"
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+
+    # Part 4: Calculating T_c by extrapolating from susceptibility peaks
+    Tc_chi = extrapolate_Tc_from_chi()
+    print("T_c extrapolated from chi: {}".format(Tc_chi))
+    with open(output_dir + "/Tc_from_chi.txt", "w") as f:
+        print("{:.4f}".format(Tc_chi), file=f)
+
+    # Part 5: Calculating \gamma / \nu from peaks in susceptibility
+    gamma_nu = gamma_over_nu()
+    print("gamma / nu from chi peaks: {}".format(gamma_nu))
+    with open(output_dir + "/gamma_nu_from_chi.txt", "w") as f:
+        print("{:.4f}".format(gamma_nu), file=f)
+
+    # Part 6: Calcluating \beta / \nu from a linear fit
+    beta_nu = beta_over_nu()
+    print("beta / nu from M(T_c): {}".format(beta_nu))
+    with open(output_dir + "/beta_nu_from_M_at_Tc.txt", "w") as f:
+        print("{:.4f}".format(beta_nu), file=f)
+
+    # Part 7: Calculating T_c from C_V
+    Tc_Cv = extrapolate_Tc_from_Cv()
+    print("T_c extrapolated from Cv: {}".format(Tc_Cv))
+    with open(output_dir + "/Tc_from_Cv.txt", "w") as f:
+        print("{:.4f}".format(Tc_Cv), file=f)
+
+    # Part 8: Computing S(infty) for different values of L
+    with open(output_dir + "/entropy_table.txt", "w") as f:
+        print("\\begin{tabular}{c|", end="", file=f)
+        for i in range(len(L_values)): print("c", end="", file=f)
+        print("}", file=f)
+        print("$L$", file=f)
+        for L in L_values:
+            print("& {}".format(L), file=f)
+        print("\\\\", file=f)
+        print("\\hline", file=f)
+        print("$S(\infty)$", file=f)
+        for L in L_values:
+            print("S(L = {}) = {}".format(L, entropy_limit(L)))
+            print("& {:S}".format(entropy_limit(L)), file=f)
+        print("\\end{tabular}", file=f)
+
+
---
+++ graphs.py
@@ -0,0 +1,209 @@
+from analysis import *
+from uncertainties import umath
+
+# Make matplotlib work with latex taken from here:
+# https://matplotlib.org/3.1.1/tutorials/text/usetex.html
+from matplotlib import rc
+rc('text', usetex=True)
+
+def plot_feature(L, feature, normalize = False):
+    filename = "L{}output.txt".format(L)
+    output = Output(filename)
+    T = output.T
+    vals = getattr(output, feature)
+    errs = getattr(output, feature + "_err")
+
+    if normalize:
+        vals = vals / L**2
+        errs = errs / L**2
+
+    plt.errorbar(T, vals, yerr=errs, label="L = {}".format(L))
+
+# Plotting energy
+for L in L_values:
+    plot_feature(L, "E", True)
+plt.title("Energy vs Temperature")
+plt.xlabel("Temperature")
+plt.ylabel("Energy per spin")
+plt.legend()
+plt.savefig("energy-plot.png")
+plt.cla()
+
+# Plotting Specific heat
+for L in L_values:
+    plot_feature(L, "C", False)
+plt.title("Specific Heat vs Temperature")
+plt.xlabel("Temperature")
+plt.ylabel("Specific Heat")
+plt.legend()
+plt.savefig("specific-heat-plot.png")
+plt.cla()
+
+# Plotting manetization
+for L in L_values:
+    plot_feature(L, "M", False)
+plt.title("Magnetization vs Temperature")
+plt.xlabel("Temperature")
+plt.ylabel("Average Magnetization")
+plt.legend()
+plt.savefig("magnetization-plot.png")
+plt.cla()
+
+
+# Plotting susceptibility
+for L in L_values:
+    plot_feature(L, "chi", False)
+plt.title("Susceptibility vs Temperature")
+plt.xlabel("Temperature")
+plt.ylabel("Susceptibility")
+plt.legend()
+plt.savefig("susceptibility-plot.png")
+plt.cla()
+
+# Plotting chi(Tc) vs L
+x = np.log(np.array(L_values))
+chi_at_Tc = [umath.log(chi_max(get_output(L))) for L in L_values]
+y = [x.nominal_value for x in chi_at_Tc]
+yerr = [x.std_dev for x in chi_at_Tc]
+plt.errorbar(x, y, yerr=yerr, fmt='o')
+
+# Plot linear fit
+slope, intercept = np.polyfit(x, y, 1)
+y = slope * x + intercept
+plt.plot(x, y, color='red')
+
+plt.title("Susceptibility as a function of $\log L$")
+plt.xlabel("$\log L$")
+plt.ylabel("$\\log \\chi(T_c(L))$")
+plt.savefig("chi-max-plot.png")
+plt.cla()
+
+# Plotting Tc(L) vs L, where Tc is taken from the susceptibility
+x = 1 / np.array(L_values)
+Tc = [Tc_from_chi(get_output(L)) for L in L_values]
+plt.scatter(x, Tc)
+
+# Plot linear fit
+slope, intercept = np.polyfit(x, Tc, 1)
+x = np.arange(0, 0.12, 0.01)
+y = slope * x + intercept
+plt.plot(x, y, color='red')
+
+plt.title("$T_c$ from susceptibility vs $L^{-1}$")
+plt.xlabel("$L^{-1}$")
+plt.ylabel("$T_c(L)$")
+plt.savefig("chi-Tc-vs-Linv-plot.png")
+plt.cla()
+
+# Plotting Tc(L) vs L, where Tc is taken from the specific heat
+x = 1 / np.array(L_values)
+Tc = [Tc_from_Cv(get_output(L)) for L in L_values]
+plt.scatter(x, Tc)
+
+# Plot linear fit
+slope, intercept = np.polyfit(x, Tc, 1)
+x = np.arange(0, 0.12, 0.01)
+y = slope * x + intercept
+plt.plot(x, y, color='red')
+
+plt.title("$T_c$ from specific heat vs $L^{-1}$")
+plt.xlabel("$L^{-1}$")
+plt.ylabel("$T_c(L)$")
+plt.savefig("Cv-Tc-vs-Linv-plot.png")
+plt.cla()
+
+# Plotting L^{-\gamma / \nu} \chi as a function of L^{1 / \nu} (T - T_c(L))
+def plot_chi_scaling(L, gamma_nu, nu):
+    output = get_output(L)
+    T = output.T
+
+    Tc = Tc_from_chi(output)
+    scaling_var = L**(1 / nu) * (T - Tc)
+    
+    chi = output.chi * L**(-gamma_nu)
+    chi_err = output.chi_err * L**(-gamma_nu)
+    
+    plt.errorbar(scaling_var, chi, yerr=chi_err, label="L = {}".format(L))
+    
+# Using exact gamma / nu = 1.75
+for L in L_values:
+    plot_chi_scaling(L, 1.75, 1)
+plt.title("$L^{-\\gamma / \\nu} \chi$ as a function of $L^{1 / \\nu} (T - T_c(L))$ ($\\gamma / \\nu$ exact)")
+plt.xlabel("$L^{1 / \\nu} (T - T_c(L))$")
+plt.ylabel("$L^{-\\gamma / \\nu} \chi$")
+plt.xlim([-25, 25])
+plt.legend()
+plt.savefig("chi-scaling-gamma-nu-exact.png")
+plt.cla()
+
+# Using gamma / nu fit from fitting log(chi(T_c)) vs. log(L)
+gamma_nu = gamma_over_nu(L_values)
+for L in L_values:
+    plot_chi_scaling(L, gamma_nu, 1)
+plt.title("$L^{-\\gamma / \\nu} \chi$ as a function of $L^{1 / \\nu} (T - T_c(L))$ ($\\gamma / \\nu$ from fit)")
+plt.xlabel("$L^{1 / \\nu} (T - T_c(L))$")
+plt.ylabel("$L^{-\\gamma / \\nu} \chi$")
+plt.legend()
+plt.xlim([-25, 25])
+plt.savefig("chi-scaling-gamma-nu-fit.png")
+plt.cla()
+
+# Plotting L^{-\beta / \nu} M as a function of L^{1 / \nu} (T - T_c(L))
+def plot_M_scaling(L, beta_nu, nu):
+    output = get_output(L)
+    T = output.T
+
+    Tc = Tc_from_chi(output)
+    scaling_var = L**(1 / nu) * (T - Tc)
+    
+    M = output.M * L**(beta_nu)
+    M_err = output.M_err * L**(beta_nu)
+    
+    plt.errorbar(scaling_var, M, yerr=M_err, label="L = {}".format(L))
+
+beta_nu = beta_over_nu(L_values)
+for L in L_values:
+    plot_M_scaling(L, beta_nu, 1)
+plt.title("$L^{-\\beta / \\nu} M$ as a function of $L^{1 / \\nu} (T - T_c(L))$ ($\\beta / \\nu$ from fit)")
+plt.xlabel("$L^{1 / \\nu} (T - T_c(L))$")
+plt.ylabel("$L^{-\\beta / \\nu} M$")
+plt.legend()
+plt.savefig("M-scaling.png")
+plt.cla()
+
+# Plotting C_V(T_c) versus \log L
+x = np.log(L_values)
+y = [Cv_max(get_output(L)).nominal_value for L in L_values]
+plt.scatter(x, y)
+plt.title("$C_V$ as a function of $\log L$")
+plt.xlabel("$\log L$")
+plt.ylabel("$C_V(T_c(L))$")
+plt.savefig("Cv-max-plot.png")
+plt.cla()
+
+# Plotting entropy vs temperature
+for L in L_values:
+    output = get_output(L)
+    x = output.T
+    y = entropy(output)
+    plt.plot(x, y, label="L = {}".format(L))
+
+plt.title("Entropy as a function of temperature")
+plt.xlabel("Temperature")
+plt.ylabel("Entropy")
+plt.legend()
+plt.savefig("entropy-plot.png")
+plt.cla()
+
+# Plotting free energy vs temperature
+for L in L_values:
+    output = get_output(L)
+    x = output.T
+    y = free_energy(output) / L**2
+    plt.plot(x, y, label="L = {}".format(L))
+
+plt.title("Free energy per spin as a function of temperature")
+plt.xlabel("Temperature")
+plt.ylabel("Free energy per spin")
+plt.legend()
+plt.savefig("free-energy-plot.png")
